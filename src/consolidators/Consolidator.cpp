6fbda0e609d73a53440712de9404b015ff79113d
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-05-13 13:16:40 +0200

    Consolidate Output symbRef separately (for now)

--- src/consolidators/Consolidator.cpp
+++ src/consolidators/Consolidator.cpp
@@ -131,6 +131,10 @@ namespace CPharmML
         
         // Ask non-symbols to set all SymbRefs to point to Symbols and to update the referencedSymbols in Referer children
         model->getModellingSteps()->gatherSymbRefs(symbIdMap);
+
+        // FIXME: Hackish, Observation should probably be a Symbol. The PharmML structure is a bit entagled need more time to think about this...
+        PharmML::SymbRef *observation = model->getModelDefinition()->getObservationModel()->getOutput();
+        observation->setSymbol(symbIdMap[observation->toString()]);
     }
 
     std::vector<CPharmML::PopulationParameter *> Consolidator::getPopulationParameters() {

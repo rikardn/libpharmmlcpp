a9f2c355bf20f15eb3b61239b16b5668eb78981f
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-05-25 13:39:47 +0200

    Fix include guard misspelling

--- src/AST/AstTransformation.h
+++ src/AST/AstTransformation.h
@@ -15,7 +15,7 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PHARMML_ASTTRANSFORMATIOM_H_
+#ifndef PHARMML_ASTTRANSFORMATION_H_
 #define PHARMML_ASTTRANSFORMATION_H_
 
 #include <vector>

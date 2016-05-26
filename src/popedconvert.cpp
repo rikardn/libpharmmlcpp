82b30afff9766a0504929d47084178b0bbe9f74b
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-05-26 11:57:43 +0200

    PopED: Split out a model conversion function from pharmml2poped
    This should be easy to call via Rcpp and using capture.output()

--- src/popedconvert.cpp
+++ src/popedconvert.cpp
@@ -1,34 +1,43 @@
 /* libpharmmlcpp - Library to handle PharmML
  * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * his library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <iostream>
+#include <exception>
+#include <PharmML/Model.h>
+#include <AST/Scalar.h>
+#include <symbols/Variable.h>
+#include <symbols/PopulationParameter.h>
+#include <generators/PopED/PopEDGenerator.h>
 
-void convert_pharmml_poped(const char *);
+using namespace PharmML;
 
-int main(int argc, char **argv)
+
+void convert_pharmml_poped(const char *filename)
 {
-    if (argc < 2) {
-        std::cout << "No PharmML file specified" << std::endl;
-        return 5;
+    Model *model;
+    try {
+        model = new Model(filename);
+    } 
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return;
     }
 
-    const char *filename = argv[1];
+    PopEDGenerator pgen; 
 
-    convert_pharmml_poped(filename);
-
-    return 0;
+    std::cout << pgen.generateModel(model) << std::endl;
 }

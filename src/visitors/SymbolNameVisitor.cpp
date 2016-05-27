838e072881550780ca4ef952ca160ce09d961e64
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-05-27 09:15:32 +0200

    New visitor SymbolNameVisitor to just return the names of symbols
    This can be a parent for similar symbol visitors that override
    some or all of the symbols.

---
+++ src/visitors/SymbolNameVisitor.cpp
@@ -0,0 +1,76 @@
+/* libpharmmlcpp - Library to handle PharmML
+ * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
+ *
+ * This library is free software { you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation { either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * his library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY { without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library { if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <visitors/SymbolNameVisitor.h>
+#include <symbols/ObservationModel.h>
+#include <symbols/PopulationParameter.h>
+#include <symbols/IndividualParameter.h>
+#include <symbols/RandomVariable.h>
+#include <symbols/VariabilityLevel.h>
+#include <symbols/IndependentVariable.h>
+#include <symbols/Variable.h>
+#include <symbols/DerivativeVariable.h>
+#include <symbols/Covariate.h>
+#include <symbols/FunctionDefinition.h>
+
+namespace PharmML {
+
+    void SymbolNameVisitor::visit(ObservationModel *node) { 
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(PopulationParameter *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(IndividualParameter *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(RandomVariable *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(VariabilityLevel *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(IndependentVariable *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(Variable *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(DerivativeVariable *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(Covariate *node) {
+        this->setValue(node->getSymbId());
+    }
+
+
+    void SymbolNameVisitor::visit(FunctionDefinition *node) {
+        this->setValue(node->getSymbId());
+    }
+
+    void SymbolNameVisitor::visit(FunctionArgumentDefinition *node) {
+        this->setValue(node->getSymbId());
+    }
+}
---
+++ src/visitors/SymbolNameVisitor.h
@@ -0,0 +1,56 @@
+/* libpharmmlcpp - Library to handle PharmML
+ * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * his library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PHARMML_SYMBOLNAMEVISITOR_H_
+#define PHARMML_SYMBOLNAMEVISITOR_H_
+
+#include <visitors/SymbolVisitor.h>
+#include <visitors/StringVisitor.h>
+
+namespace PharmML
+{
+    class ObservationModel;
+    class PopulationParameter;
+    class IndividualParameter;
+    class RandomVariable;
+    class VariabilityLevel;
+    class Variable;
+    class DerivativeVariable;
+    class IndependentVariable;
+    class Covariate;
+
+    class FunctionDefinition;
+    class FunctionArgumentDefinition;
+
+    class SymbolNameVisitor : public SymbolVisitor, public StringVisitor
+    {
+        public:
+            void visit(ObservationModel *node) override;
+            void visit(PopulationParameter *node) override;
+            void visit(IndividualParameter *node) override;
+            void visit(RandomVariable *node) override;
+            void visit(VariabilityLevel *node) override;
+            void visit(IndependentVariable *node) override;
+            void visit(Variable *node) override;
+            void visit(DerivativeVariable *node) override;
+            void visit(Covariate *node) override;
+            void visit(FunctionDefinition *node) override;
+            void visit(FunctionArgumentDefinition *node) override;
+    };
+}
+
+#endif

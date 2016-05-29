1069ce8319f22e2340c8d50184c0d8e8144c7e70
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-05-29 16:36:53 +0200

    Add SymbolSortVisitor to simplify type sorting of symbol sets

---
+++ src/visitors/SymbolSortVisitor.cpp
@@ -0,0 +1,69 @@
+/* libpharmmlcpp - Library to handle PharmML
+ * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
+ *
+ * This library is free software { you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation { either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * his library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY { without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library { if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <visitors/SymbolSortVisitor.h>
+#include <symbols/ObservationModel.h>
+#include <symbols/PopulationParameter.h>
+#include <symbols/IndividualParameter.h>
+#include <symbols/RandomVariable.h>
+#include <symbols/VariabilityLevel.h>
+#include <symbols/IndependentVariable.h>
+#include <symbols/Variable.h>
+#include <symbols/DerivativeVariable.h>
+#include <symbols/Covariate.h>
+#include <symbols/FunctionDefinition.h>
+
+namespace PharmML {
+
+    SymbolSet SymbolSortVisitor::getDerivatives() {
+        return this->derivatives;
+    }
+
+    void SymbolSortVisitor::visit(ObservationModel *node) { 
+    }
+
+    void SymbolSortVisitor::visit(PopulationParameter *node) {
+    }
+
+    void SymbolSortVisitor::visit(IndividualParameter *node) {
+    }
+
+    void SymbolSortVisitor::visit(RandomVariable *node) {
+    }
+
+    void SymbolSortVisitor::visit(VariabilityLevel *node) {
+    }
+
+    void SymbolSortVisitor::visit(IndependentVariable *node) {
+    }
+
+    void SymbolSortVisitor::visit(Variable *node) {
+    }
+
+    void SymbolSortVisitor::visit(DerivativeVariable *node) {
+        this->derivatives.addSymbol(node);
+    }
+
+    void SymbolSortVisitor::visit(Covariate *node) {
+    }
+
+    void SymbolSortVisitor::visit(FunctionDefinition *node) {
+    }
+
+    void SymbolSortVisitor::visit(FunctionArgumentDefinition *node) {
+    }
+}
---
+++ src/visitors/SymbolSortVisitor.h
@@ -0,0 +1,61 @@
+/* libpharmmlcpp - Library to handle PharmML
+ * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * his library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PHARMML_SYMBOLSORTVISITOR_H_
+#define PHARMML_SYMBOLSORTVISITOR_H_
+
+#include <visitors/SymbolVisitor.h>
+#include <symbols/SymbolSet.h>
+
+namespace PharmML
+{
+    class ObservationModel;
+    class PopulationParameter;
+    class IndividualParameter;
+    class RandomVariable;
+    class VariabilityLevel;
+    class Variable;
+    class DerivativeVariable;
+    class IndependentVariable;
+    class Covariate;
+
+    class FunctionDefinition;
+    class FunctionArgumentDefinition;
+
+    class SymbolSortVisitor : public SymbolVisitor
+    {
+        public:
+            SymbolSet getDerivatives();
+
+            void visit(ObservationModel *node) override;
+            void visit(PopulationParameter *node) override;
+            void visit(IndividualParameter *node) override;
+            void visit(RandomVariable *node) override;
+            void visit(VariabilityLevel *node) override;
+            void visit(IndependentVariable *node) override;
+            void visit(Variable *node) override;
+            void visit(DerivativeVariable *node) override;
+            void visit(Covariate *node) override;
+            void visit(FunctionDefinition *node) override;
+            void visit(FunctionArgumentDefinition *node) override;
+
+        private:
+            SymbolSet derivatives;
+    };
+}
+
+#endif

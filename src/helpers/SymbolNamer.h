c6105d12f31f40973a59f16e3a7ab0fbce3372d9
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2016-06-07 23:04:48 +0200

    Init SymbolNamer (mangler of names for end-tool satisfaction)
    Semi-supporting of Unicode.

---
+++ src/helpers/SymbolNamer.h
@@ -0,0 +1,74 @@
+/* libpharmmlcpp - Library to handle PharmML
+ * Copyright (C) 2016 Rikard Nordgren and Gunnar Yngman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * his library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PHARMML_SYMBOLNAMER_H_
+#define PHARMML_SYMBOLNAMER_H_
+
+#include <unordered_map>
+#include <unordered_set>
+#include <codecvt>
+#include <locale>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include <symbols/Symbol.h>
+
+namespace PharmML
+{
+    namespace LatinChars {
+        const std::unordered_set<char32_t> DIGITS      = {'0','1','2','3','4','5','6','7','8','9'};
+        const std::unordered_set<char32_t> LOWER_ALPHA = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        const std::unordered_set<char32_t> UPPER_ALPHA = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        const std::unordered_set<char32_t> UNDERSCORE  = {'_'};
+        const std::unordered_set<char32_t> PERIOD      = {'.'};
+        const std::unordered_set<char32_t> HYPHEN      = {'-'};
+    };
+
+    class SymbolNamer
+    {
+        public:
+            SymbolNamer(std::unordered_set<std::u32string> reserved_words);
+
+            // Set target tool restrictions
+            void addCharSet(std::unordered_set<char32_t> chars);
+            void addInitialCharSet(std::unordered_set<char32_t> chars);
+            void setReservedPrefix(std::u32string prefix);
+
+            // Name getter
+            std::u32string getName(Symbol *symbol);
+
+        private:
+            // Target tool restrictions
+            std::unordered_set<std::u32string> illegal_words;
+            std::unordered_set<char32_t> legal_chars;
+            std::unordered_set<char32_t> legal_initial_chars;
+            std::u32string reserved_prefix;
+
+            // Earlier generated names
+            std::unordered_set<std::u32string> names;
+            std::unordered_map<Symbol*, std::u32string> name_map;
+
+            // Private helper functions
+            bool charInSet(char32_t ch, const std::unordered_set<char32_t> &ch_set);
+            char32_t substituteStandardChar(char32_t ch);
+            std::u32string avoidCollision(std::u32string name, const std::unordered_set<std::u32string> &illegals);
+            std::u32string stringFromNumerals(uint num, const std::vector<char32_t> &numerals);
+    };
+}
+
+#endif

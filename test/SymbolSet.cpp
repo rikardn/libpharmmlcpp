3033ee1621bd07bb8ba5a6dfd05314d5c7d5711e
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-06-07 10:14:06 +0200

    Chang old Dependencies unit test into SymbolSet unit test

--- test/SymbolSet.cpp
+++ test/SymbolSet.cpp
@@ -1,62 +1,41 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "../Dependencies.h"
-#include "../Model.h"
+#include <symbols/Symbol.h>
+#include <symbols/SymbolSet.h>
+#include <symbols/PopulationParameter.h>
 
-TEST_CASE("Dependencies class", "[Dependencies]") {
-    PharmML::Dependencies d;
+TEST_CASE("SymbolSet class", "[SymbolSet]") {
+    PharmML::SymbolSet ss;
 
-    REQUIRE(!d.hasDependency("CL"));
-    REQUIRE(!d.hasDependency("V"));
-    REQUIRE(d.numDependencies() == 0);
+    PharmML::PopulationParameter cl("CL");
+    PharmML::PopulationParameter v("V");
 
-    SECTION("Add one dependency") {
-        d.addDependency("CL");
+    REQUIRE(!ss.hasSymbol(&cl));
+    REQUIRE(!ss.hasSymbol(&v));
+    REQUIRE(ss.isEmpty());
 
-        REQUIRE(d.hasDependency("CL"));
-        REQUIRE(!d.hasDependency("V"));
-        REQUIRE(d.numDependencies() == 1);
+    SECTION("Add one symbol") {
+        ss.addSymbol(&cl);
+
+        REQUIRE(ss.hasSymbol(&cl));
+        REQUIRE(!ss.hasSymbol(&v));
+        REQUIRE(ss.numSymbols() == 1);
     }
-    SECTION("Re-add same dependency") {
-        d.addDependency("CL");
-        d.addDependency("CL");
+    SECTION("Re-add same symbol") {
+        ss.addSymbol(&cl);
+        ss.addSymbol(&cl);
 
-        REQUIRE(d.hasDependency("CL"));
-        REQUIRE(!d.hasDependency("V"));
-        REQUIRE(d.numDependencies() == 1);
+        REQUIRE(ss.hasSymbol(&cl));
+        REQUIRE(!ss.hasSymbol(&v));
+        REQUIRE(ss.numSymbols() == 1);
     }
-    SECTION("Add two dependencies") {
-        d.addDependency("V");
-        d.addDependency("CL");
+    SECTION("Add two symbols") {
+        ss.addSymbol(&v);
+        ss.addSymbol(&cl);
 
-        REQUIRE(d.hasDependency("CL"));
-        REQUIRE(d.hasDependency("V"));
-        REQUIRE(d.numDependencies() == 2);
-    }
-}
-
-TEST_CASE("Dependencies of variable", "[Dependencies]") {
-    auto *model = new PharmML::Model("testfiles/UseCase2.xml");
-    
-    REQUIRE(model);
-    REQUIRE(model->getModelDefinition());
-    REQUIRE(model->getModelDefinition()->getStructuralModel());
-    REQUIRE(model->getModelDefinition()->getStructuralModel()->getVariables().size() == 3);
-    PharmML::Dependencies& deps1 = model->getModelDefinition()->getStructuralModel()->getVariables()[1]->getDependencies();
-    PharmML::Dependencies& deps2 = model->getModelDefinition()->getStructuralModel()->getVariables()[2]->getDependencies();
-
-    SECTION("Check dependencies of UseCase2") {
-        REQUIRE(deps1.numDependencies() == 2);
-        REQUIRE(deps1.hasDependency("CL"));
-        REQUIRE(deps1.hasDependency("V"));
-
-        REQUIRE(deps2.numDependencies() == 6);
-        REQUIRE(deps2.hasDependency("T"));
-        REQUIRE(deps2.hasDependency("TLAG"));
-        REQUIRE(deps2.hasDependency("D"));
-        REQUIRE(deps2.hasDependency("V"));
-        REQUIRE(deps2.hasDependency("KA"));
-        REQUIRE(deps2.hasDependency("k"));
+        REQUIRE(ss.hasSymbol(&cl));
+        REQUIRE(ss.hasSymbol(&v));
+        REQUIRE(ss.numSymbols() == 2);
     }
 }

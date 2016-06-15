b6d63cb5970ebf56c9a501f56368be7d75319e21
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2016-06-15 17:08:59 +0200

    Add ast_parenthesizer.xml test file
    Also stop ignoring directory testfiles. End of edit war?

--- test/AstParenthesizer.cpp
+++ test/AstParenthesizer.cpp
@@ -1,8 +1,10 @@
 #include "catch.hpp"
 
+#include <PharmML/PharmML.h>
 #include <visitors/AstParenthesizer.h>
 
 TEST_CASE("AstParenthesizer", "[AstParenthesizer]") {
+    pharmmlcpp::PharmML model("testfiles/ast_parenthesizer.xml");
     pharmmlcpp::AstParenthesizer ap;
     SECTION("WIP") {
        
